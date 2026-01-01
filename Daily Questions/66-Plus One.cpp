dce069a132b5bf6dcb30f397fe7c5e41a2afdf94
Piyush <piyushpatil0031@gmail.com>
2026-01-01 22:19:13 +0530

    daily que done

---
+++ Daily Questions/66-Plus One.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int carry = 1;
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+        if (carry)
+        {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
