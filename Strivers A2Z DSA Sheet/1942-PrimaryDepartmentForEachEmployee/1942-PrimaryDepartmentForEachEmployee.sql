d6eb29c551498149eb993dc8a59f89550a831f39
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:07:39 +0530

    Create 1942-PrimaryDepartmentForEachEmployee

---
+++ Strivers A2Z DSA Sheet/1942-PrimaryDepartmentForEachEmployee/1942-PrimaryDepartmentForEachEmployee.sql
@@ -0,0 +1,11 @@
+-- Last updated: 8/7/2026, 7:07:39 PM
+# Write your MySQL query statement below
+select employee_id, department_id
+from Employee
+where primary_flag = 'Y'
+group by employee_id
+Union
+select employee_id, department_id
+from Employee
+group by employee_id
+having count(*) = 1;
