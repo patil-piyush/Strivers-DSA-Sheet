8afdcb24329c92c1af1952c95503378066660bf1
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:12:56 +0530

    Create 283-MoveZeroes

---
+++ Strivers A2Z DSA Sheet/283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/7/2026, 7:12:55 PM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0, j=0;
+        while(i<nums.size()){
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+            i++;
+        }
+    }
+};
