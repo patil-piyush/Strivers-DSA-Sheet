8a758aaa462fc7342f118320792f31ca9e32b2e7
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:08:54 +0530

    Create 1395-MinimumTimeVisitingAllPoints

---
+++ Strivers A2Z DSA Sheet/1395-MinimumTimeVisitingAllPoints/1395-MinimumTimeVisitingAllPoints.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/7/2026, 7:08:54 PM
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            int currX = points[i][0];
+            int currY = points[i][1];
+            int targetX = points[i + 1][0];
+            int targetY = points[i + 1][1];
+            ans += max(abs(targetX - currX), abs(targetY - currY));
+        }
+
+        return ans;
+    }
+};
