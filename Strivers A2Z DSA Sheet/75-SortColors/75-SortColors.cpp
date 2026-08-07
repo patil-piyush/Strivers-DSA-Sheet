5d3294a1a8f79d7393708cd775e9851dc18a8d24
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:16:04 +0530

    Create 75-SortColors

---
+++ Strivers A2Z DSA Sheet/75-SortColors/75-SortColors.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/7/2026, 7:16:03 PM
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int mid=0, low=0, high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            } 
+            else if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
