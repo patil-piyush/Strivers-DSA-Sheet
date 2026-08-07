f669354ed3819c9587b65b6674a5692708206f5b
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:14:31 +0530

    Create 153-FindMinimumInRotatedSortedArray

---
+++ Strivers A2Z DSA Sheet/153-FindMinimumInRotatedSortedArray/153-FindMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/7/2026, 7:14:30 PM
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0; 
+        int high = nums.size()-1;
+        int ans = INT_MAX;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[low]<=nums[high]){
+                ans = min(ans, nums[low]);
+                return ans;
+            }
+
+            if(nums[low]<=nums[mid]){
+                ans = min(ans, nums[low]);
+                low = mid+1;
+            }
+            else{
+                ans = min(ans, nums[mid]);
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
