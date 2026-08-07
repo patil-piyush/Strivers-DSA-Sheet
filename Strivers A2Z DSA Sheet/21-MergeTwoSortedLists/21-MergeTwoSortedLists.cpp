03a60035334c9f1457d5ef4449a1db03d384586a
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:17:24 +0530

    Create 21-MergeTwoSortedLists

---
+++ Strivers A2Z DSA Sheet/21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -0,0 +1,52 @@
+// Last updated: 8/7/2026, 7:17:24 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+        ListNode* head = new ListNode;
+        ListNode* tail = new ListNode;
+        if (list1->val >= list2->val) {
+            head = list2;
+            tail = list2;
+            list2 = list2->next;
+        } else {
+            head = list1;
+            tail = list1;
+            list1 = list1->next;
+        }
+
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val >= list2->val) {
+                tail->next = list2;
+                tail = tail->next;
+                list2 = list2->next;
+            } else {
+                tail->next = list1;
+                tail = tail->next;
+                list1 = list1->next;
+            }
+        }
+        while (list1 != NULL) {
+            tail->next = list1;
+            tail = tail->next;
+            list1 = list1->next;
+        }
+        while (list2 != NULL) {
+            tail->next = list2;
+            tail = tail->next;
+            list2 = list2->next;
+        }
+        return head;
+    }
+};
