23c8379cd38177efe2a079ad3faee6f08ce0b02e
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:07:32 +0530

    Create 1956-MaximumElementAfterDecreasingAndRearranging

---
+++ Strivers A2Z DSA Sheet/1956-MaximumElementAfterDecreasingAndRearranging/1956-MaximumElementAfterDecreasingAndRearranging.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/7/2026, 7:07:32 PM
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        arr[0] = 1;
+        for(int i=0; i<arr.size()-1; i++){
+            if(abs(arr[i] - arr[i+1]) > 1){
+                arr[i+1] = arr[i] + 1;
+            }
+        }
+        return arr[arr.size()-1];
+    }
+};
