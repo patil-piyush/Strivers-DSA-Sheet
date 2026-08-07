988dc3b71140e0e3744037afc4ee52532a22ace6
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:13:23 +0530

    Create 222-CountCompleteTreeNodes

---
+++ Strivers A2Z DSA Sheet/222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/7/2026, 7:13:23 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        int x = countNodes(root->right);
+        int y = countNodes(root->left);
+        return x+y+1;
+    }
+};
