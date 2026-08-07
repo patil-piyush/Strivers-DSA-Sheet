319469628a5ec0f560830cf54f8b7c7e0cf4afd4
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:07:26 +0530

    Create 2032-LargestOddNumberInString

---
+++ Strivers A2Z DSA Sheet/2032-LargestOddNumberInString/2032-LargestOddNumberInString.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/7/2026, 7:07:26 PM
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int end = -1;
+        int i;
+
+        for(int i  = num.size()-1; i>=0; i--){
+            if((num[i] - '0')%2 == 1){
+                end = i;
+                break; 
+            }
+        }
+        
+
+        i = 0;
+        while(i<=end && num[i]=='0') i++;
+
+        return num.substr(i, end - i+1);
+    }
+};
