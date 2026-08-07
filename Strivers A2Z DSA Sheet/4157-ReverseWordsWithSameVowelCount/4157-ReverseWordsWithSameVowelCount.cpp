f4b6920e9321014835804bc6a6079be03b8281ce
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:04:45 +0530

    Create 4157-ReverseWordsWithSameVowelCount

---
+++ Strivers A2Z DSA Sheet/4157-ReverseWordsWithSameVowelCount/4157-ReverseWordsWithSameVowelCount.cpp
@@ -0,0 +1,38 @@
+// Last updated: 8/7/2026, 7:04:44 PM
+class Solution {
+public:
+    string reverseWords(string s) {
+        int vowelCount = 0;
+        int i = 0;
+        while (i < s.size() && s[i] != ' '){
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                vowelCount += 1;
+            }
+            i += 1;
+        }
+        i += 1;
+        while (i < s.size()) {
+            int j = i, k = i, count = 0;
+            char temp;
+            while (s[k] != ' ' && k < s.size()) {
+                if (s[k] == 'a' || s[k] == 'e' || s[k] == 'i' || s[k] == 'o' || s[k] == 'u') {
+                    count += 1;
+                }
+                k += 1;
+            }
+            i = k + 1;
+            k--;
+            if (count == vowelCount) {
+                while (k > j) {
+                    temp = s[k];
+                    s[k] = s[j];
+                    s[j] = temp;
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return s;
+    }
+
+};
