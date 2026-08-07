b6a8621cf93bb200f724d98e3ec1cd492d19f0c9
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:10:17 +0530

    Create 981-DeleteColumnsToMakeSorted

---
+++ Strivers A2Z DSA Sheet/981-DeleteColumnsToMakeSorted/981-DeleteColumnsToMakeSorted.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/7/2026, 7:10:16 PM
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int rows = strs.size();
+        int cols = strs[0].size();
+        int count = 0;
+
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < rows - 1; i++) {
+                if (strs[i][j] > strs[i + 1][j]) {
+                    count++;
+                    break; 
+                }
+            }
+        }
+        return count;
+    }
+};
