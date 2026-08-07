b0059e593eba4009b40069234a87840a66465abb
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:07:41 +0530

    Create 1892-FindTotalTimeSpentByEachEmployee

---
+++ Strivers A2Z DSA Sheet/1892-FindTotalTimeSpentByEachEmployee/1892-FindTotalTimeSpentByEachEmployee.sql
@@ -0,0 +1,5 @@
+-- Last updated: 8/7/2026, 7:07:41 PM
+# Write your MySQL query statement below
+select event_day as day, emp_id, sum(out_time - in_time) as total_time
+from Employees
+group by emp_id, day;
