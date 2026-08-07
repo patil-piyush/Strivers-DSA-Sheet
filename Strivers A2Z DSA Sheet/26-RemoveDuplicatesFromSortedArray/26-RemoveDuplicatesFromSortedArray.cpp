ea70119172c3d87069877ea230afca14da7c4739
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:17:20 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ Strivers A2Z DSA Sheet/26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/7/2026, 7:17:19 PM
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+
+        int k = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
