d42e54397ad695cb5092942eaca93a4d9fd17a72
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:14:03 +0530

    Create 181-EmployeesEarningMoreThanTheirManagers

---
+++ Strivers A2Z DSA Sheet/181-EmployeesEarningMoreThanTheirManagers/181-EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,6 @@
+-- Last updated: 8/7/2026, 7:14:03 PM
+# Write your MySQL query statement below
+select e.name as Employee
+from Employee e
+left join Employee a on e.managerId = a.id 
+where e.salary > a.salary;
