255f649062dbaf37c20eee20c6ddc9b29ab2b1fd
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:17:00 +0530

    Create 39-CombinationSum

---
+++ Strivers A2Z DSA Sheet/39-CombinationSum/39-CombinationSum.cpp
@@ -0,0 +1,31 @@
+// Last updated: 8/7/2026, 7:17:00 PM
+class Solution {
+    vector<vector<int>> result;
+
+public:
+    void combination(vector<int>& candidates, vector<int>& ds, int index, int target) {
+
+        if (target == 0) {
+            result.push_back(ds);
+            return;
+        }
+
+        if (index == candidates.size()) {
+            return;
+        }
+
+        if (target >= candidates[index]) {
+            ds.push_back(candidates[index]);
+            combination(candidates, ds, index, target - candidates[index]);
+            ds.pop_back();  
+        }
+
+        combination(candidates, ds, index + 1, target);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> ds;
+        combination(candidates, ds, 0, target);
+        return result;
+    }
+};
