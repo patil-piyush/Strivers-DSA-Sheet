054276149b45c4724aac840747aa4073694e871b
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:15:20 +0530

    Create 110-BalancedBinaryTree

---
+++ Strivers A2Z DSA Sheet/110-BalancedBinaryTree/110-BalancedBinaryTree.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/7/2026, 7:15:19 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(!root) return 0;
+        int right = height(root->right);
+        if(right==-1) return -1;
+        int left = height(root->left);
+        if(left == -1) return -1;
+        if(abs(left-right)>1) return -1;
+        return max(left,right)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(height(root) == -1) return false;
+        return true;
+    }
+};
