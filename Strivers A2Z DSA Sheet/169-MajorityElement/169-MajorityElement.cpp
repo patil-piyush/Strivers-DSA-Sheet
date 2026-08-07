3615cc98343a205d78b7b69cff9a8682f1adfe44
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:14:19 +0530

    Create 169-MajorityElement

---
+++ Strivers A2Z DSA Sheet/169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,8 @@
+// Last updated: 8/7/2026, 7:14:18 PM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+    }
+};
