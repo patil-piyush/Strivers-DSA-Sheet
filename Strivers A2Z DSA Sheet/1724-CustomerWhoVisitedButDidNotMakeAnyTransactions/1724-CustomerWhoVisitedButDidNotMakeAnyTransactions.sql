b9cd0b26bac3456994b41c0ec789126338edf904
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:08:09 +0530

    Create 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions

---
+++ Strivers A2Z DSA Sheet/1724-CustomerWhoVisitedButDidNotMakeAnyTransactions/1724-CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,7 @@
+-- Last updated: 8/7/2026, 7:08:09 PM
+# Write your MySQL query statement below
+select distinct v.customer_id, count(v.customer_id) as count_no_trans
+from Visits v
+left join Transactions t on v.visit_id = t.visit_id
+where t.transaction_id is null
+group by v.customer_id;
