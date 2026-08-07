f1981bf3020dd7b040db70efec53d44fa21fec95
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:17:44 +0530

    Create 11-ContainerWithMostWater

---
+++ Strivers A2Z DSA Sheet/11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/7/2026, 7:17:44 PM
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size()-1;
+        int maxwater=0;
+        while(l<r){
+            int area = min(height[l], height[r]) * (r-l);
+            maxwater = max(maxwater, area);
+            if(height[l]<=height[r]) l++;
+            else r--;
+        }
+        return maxwater;
+    }
+};
