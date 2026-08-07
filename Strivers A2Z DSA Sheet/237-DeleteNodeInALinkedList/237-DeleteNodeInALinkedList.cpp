d667e1820e8d4702a081df395942f0c4bab7fcc4
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:13:08 +0530

    Create 237-DeleteNodeInALinkedList

---
+++ Strivers A2Z DSA Sheet/237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/7/2026, 7:13:08 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* t = node->next;
+        node->val = t->val;
+        node->next = t->next;
+        delete t;
+    }
+};
