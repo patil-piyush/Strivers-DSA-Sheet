f281b9e85190cefbd9f4a4cbf3fa4cab185ac1d2
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-07 19:11:57 +0530

    Create 547-NumberOfProvinces

---
+++ Strivers A2Z DSA Sheet/547-NumberOfProvinces/547-NumberOfProvinces.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/7/2026, 7:11:56 PM
+class Solution {
+public:
+    void dfs(int i, vector<vector<int>>& adj, vector<int>& vis){
+        vis[i] = 1;
+
+        for(int j = 0; j < adj.size(); j++){
+            if(adj[i][j] == 1 && !vis[j]){
+                dfs(j, adj, vis);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<int> vis(isConnected.size(), 0);
+
+        int cnt = 0;
+        for(int i = 0; i < isConnected.size(); i++){
+            if(!vis[i]){
+                cnt++;
+                dfs(i, isConnected, vis);
+            }
+        }
+        return cnt;
+    }
+};
