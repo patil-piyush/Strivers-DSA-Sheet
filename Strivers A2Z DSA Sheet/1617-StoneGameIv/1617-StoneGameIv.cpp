983a5b0c2714e7bac6b6c5c7e82175e805636572
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-11 11:49:04 +0530

    Create 1617-StoneGameIv

---
+++ Strivers A2Z DSA Sheet/1617-StoneGameIv/1617-StoneGameIv.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 11:49:04 AM
+class Solution {
+public:
+    static constexpr int MAX = 100000;
+    inline static bitset<MAX + 1> dp;
+
+    inline static bool init = []() {
+        for (int i = 0; i <= MAX; i++) {
+            if (dp.test(i)) continue;
+            for (int j = 1; j * j <= MAX - i; j++)
+                dp.set(i + j * j);
+        }
+        
+        return false;
+    }();
+
+    bool winnerSquareGame(int n) { return dp.test(n); }
+};
