0f9358b35d65d5e8fbe4fca7247fccaa5a0dc104
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-08-19 13:06:21 +0530

    Create 2156-StoneGameIx

---
+++ Strivers A2Z DSA Sheet/2156-StoneGameIx/2156-StoneGameIx.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/19/2026, 1:06:21 PM
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int f[3] = {0, 0, 0};
+
+        for (auto& s : stones)
+            f[s % 3]++;
+
+        if (~f[0] & 1)
+            return min(f[1], f[2]) >= 1;
+
+        return abs(f[1] - f[2]) >= 3;
+    }
+};
