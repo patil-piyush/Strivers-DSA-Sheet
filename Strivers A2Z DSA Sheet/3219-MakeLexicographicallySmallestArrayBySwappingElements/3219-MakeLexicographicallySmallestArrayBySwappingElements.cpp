76801ba0b31e3f7950f3324d7c876961a86e6373
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-01 10:44:02 +0530

    Create 3219-MakeLexicographicallySmallestArrayBySwappingElements

---
+++ Strivers A2Z DSA Sheet/3219-MakeLexicographicallySmallestArrayBySwappingElements/3219-MakeLexicographicallySmallestArrayBySwappingElements.cpp
@@ -0,0 +1,30 @@
+// Last updated: 9/1/2026, 10:44:02 AM
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& A, int limit) {
+        vector<int> sorted = A;
+        ranges::sort(sorted);
+        vector<vector<int>> grps;
+        unordered_map<int, int> map;
+        int id = -1;
+
+        for (int i = 0; i < sorted.size(); i++) {
+            if (i == 0 || sorted[i] - sorted[i - 1] > limit) {
+                grps.push_back({});
+                id++;
+            }
+            grps[id].push_back(sorted[i]);
+            map[sorted[i]] = id;
+        }
+
+        vector<int> idx(grps.size(), 0);
+
+        for (int i = 0; i < A.size(); i++) {
+            int cur = map[A[i]];
+            A[i] = grps[cur][idx[cur]];
+            idx[cur]++;
+        }
+
+        return A;
+    }
+};
