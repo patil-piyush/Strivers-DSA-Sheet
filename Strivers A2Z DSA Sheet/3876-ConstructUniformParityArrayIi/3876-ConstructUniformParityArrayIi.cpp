78329cb3cf57f39d5979b962f43eb2fcc706ffc2
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:26:20 +0530

    Create 3876-ConstructUniformParityArrayIi

---
+++ Strivers A2Z DSA Sheet/3876-ConstructUniformParityArrayIi/3876-ConstructUniformParityArrayIi.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/8/2026, 2:26:20 PM
+class Solution {
+public:
+    bool uniformArray(vector<int>& A) {
+        int xmin = A[0];
+        bool odd = 0;
+
+        for (auto& x : A) {
+            xmin = min(xmin, x);
+            odd |= x & 1;
+        }
+
+        return (xmin & 1) == odd;
+    }
+};
