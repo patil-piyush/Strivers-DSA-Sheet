4ad7d2743b93ab942a85a9bbc7eb0ff72671ee14
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:30:42 +0530

    Create 2220-MinimumBitFlipsToConvertNumber

---
+++ Strivers A2Z DSA Sheet/2220-MinimumBitFlipsToConvertNumber/2220-MinimumBitFlipsToConvertNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 9/8/2026, 2:30:42 PM
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int ans = start ^ goal;
+        
+        int cnt = 0;
+        
+        while(ans>1){
+            cnt += ans&1;
+            ans  = ans>>1;
+        }
+        if(ans==1) cnt++;
+        return cnt;
+    }
+};
