1e7fc6945a84cc383548e5d70ed0d7cb906f8929
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:35:38 +0530

    Create 1068-ProductSalesAnalysisI

---
+++ Strivers A2Z DSA Sheet/1068-ProductSalesAnalysisI/1068-ProductSalesAnalysisI.sql
@@ -0,0 +1,5 @@
+-- Last updated: 9/8/2026, 2:35:38 PM
+# Write your MySQL query statement below
+select p.product_name, s.year, s.price
+from Sales s 
+left join Product p on s.product_id = p.product_id;
