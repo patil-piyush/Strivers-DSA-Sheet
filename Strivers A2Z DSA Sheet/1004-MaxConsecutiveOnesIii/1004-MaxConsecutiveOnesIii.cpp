1d1e8ed3c63e88b3eb63cc4bdae11bc631e304aa
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:35:58 +0530

    Create 1004-MaxConsecutiveOnesIii

---
+++ Strivers A2Z DSA Sheet/1004-MaxConsecutiveOnesIii/1004-MaxConsecutiveOnesIii.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/8/2026, 2:35:58 PM
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l=0, r=0, maxlen=0, len=0, zero=0;
+        while(r< nums.size()){
+            if(nums[r] == 0) zero++;
+            if(zero>k){
+                if(nums[l] == 0) zero--;
+                l++;
+            }
+            len = r-l+1;
+            maxlen = max(len, maxlen);
+            r++;
+        }
+        return maxlen;
+    }
+};
