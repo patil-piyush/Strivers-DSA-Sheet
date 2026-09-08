b3c3609b8d0ab551bf2dfdf0111fac5a69ffa0f2
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:33:49 +0530

    Create 1423-MaximumPointsYouCanObtainFromCards

---
+++ Strivers A2Z DSA Sheet/1423-MaximumPointsYouCanObtainFromCards/1423-MaximumPointsYouCanObtainFromCards.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/8/2026, 2:33:49 PM
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int lsum=0, rsum=0, maxsum = 0;
+        for(int i=0; i<k; i++) lsum = lsum+cardPoints[i];
+        maxsum = lsum;
+
+        for(int i=k-1, rindex=cardPoints.size()-1; i>=0; i--){
+            lsum = lsum-cardPoints[i];
+            rsum = rsum+cardPoints[rindex];
+            rindex = rindex - 1;
+            maxsum = max(maxsum, lsum+rsum);
+        }
+        return maxsum;
+    }
+};
