21a955dbdf3b4a9d74a0ae164a166be59c25a43f
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:35:32 +0530

    Create 1075-ProjectEmployeesI

---
+++ Strivers A2Z DSA Sheet/1075-ProjectEmployeesI/1075-ProjectEmployeesI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 9/8/2026, 2:35:31 PM
+# Write your MySQL query statement below
+select p.project_id, round(avg(e.experience_years),2) as average_years
+from Project p
+left join Employee e on p.employee_id = e.employee_id
+group by p.project_id;
