07c2d37fdecb2e5bd7c01ad827a1eefab9585c57
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:31:33 +0530

    Create 1967-NumberOfStringsThatAppearAsSubstringsInWord

---
+++ Strivers A2Z DSA Sheet/1967-NumberOfStringsThatAppearAsSubstringsInWord/1967-NumberOfStringsThatAppearAsSubstringsInWord.cpp
@@ -0,0 +1,12 @@
+// Last updated: 9/8/2026, 2:31:33 PM
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int n = patterns.size();
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            if(word.contains(patterns[i])) cnt++;
+        }
+        return cnt;
+    }
+};
