1d051f44308a56450cf294e0e874cf02584841a5
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2026-09-08 14:34:23 +0530

    Create 1344-AngleBetweenHandsOfAClock

---
+++ Strivers A2Z DSA Sheet/1344-AngleBetweenHandsOfAClock/1344-AngleBetweenHandsOfAClock.cpp
@@ -0,0 +1,8 @@
+// Last updated: 9/8/2026, 2:34:23 PM
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double angle = abs(30.0 * hour - 5.5 * minutes);
+        return min(angle, 360.0 - angle);
+    }
+};
